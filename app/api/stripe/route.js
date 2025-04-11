6dd5961d075daaaa62b2bfc7bdd08a175ca2646b
mayank <mayank.thapliyal1@gmail.com>
2025-04-11 12:11:27 +0530

    Signature changed for stripe

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export async function POST(request) {
   try {
     const body = await request.text();
-    const sig = request.headers.get("stripe-signature");
+    const sig = request.headers.get("Stripe-Signature");
     const event = stripe.webhooks.constructEvent(
       body,
       sig,
