1df2f4cfeda912ae3bb93b9aca2f34255edebdc7
mayank <mayank.thapliyal1@gmail.com>
2025-04-12 17:14:56 +0530

    save

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export async function POST(request) {
   try {
     const body = await request.text();
-    const sig = request.headers["stripe-signature"];
+    const sig = request.headers.get("stripe-signature");
     const event = stripe.webhooks.constructEvent(
       body,
       sig,
